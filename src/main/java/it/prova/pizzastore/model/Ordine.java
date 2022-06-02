c9a9c13633d4a865f97327d8992ee8ef6a1857d1
Luciacal01 <luciacalabria80@gmail.com>
2022-06-02 14:38:47 +0200

    2° commit: modifiche al pom.xml e creazione classi di model

---
+++ src/main/java/it/prova/pizzastore/model/Ordine.java
@@ -0,0 +1,148 @@
+package it.prova.pizzastore.model;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "ordine")
+public class Ordine {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "data")
+	private Date data;
+	@Column(name = "costototaleordine")
+	private int costoTotaleOrdine;
+	@Column(name = "closed")
+	private boolean closed;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "cliente_id", nullable = false)
+	private Cliente cliente;
+
+	@ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE }, fetch = FetchType.LAZY)
+	@JoinTable(name = "ordine_pizza", joinColumns = @JoinColumn(name = "ordine_id", referencedColumnName = "ID"), inverseJoinColumns = @JoinColumn(name = "pizza_id", referencedColumnName = "ID"))
+	private Set<Pizza> pizze;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "utente_id", nullable = false)
+	private Utente utente;
+
+	@CreationTimestamp
+	private LocalDateTime createDateTime;
+	@UpdateTimestamp
+	private LocalDateTime updateDateTime;
+
+	public Ordine() {
+	}
+
+	public Ordine(Date data, int costoTotaleOrdine, Cliente cliente, Set<Pizza> pizze, Utente utente) {
+		super();
+		this.data = data;
+		this.costoTotaleOrdine = costoTotaleOrdine;
+		this.cliente = cliente;
+		this.pizze = pizze;
+		this.utente = utente;
+	}
+
+	public Ordine(Date data, int costoTotaleOrdine, boolean closed, Cliente cliente, Set<Pizza> pizze, Utente utente) {
+		super();
+		this.data = data;
+		this.costoTotaleOrdine = costoTotaleOrdine;
+		this.closed = closed;
+		this.cliente = cliente;
+		this.pizze = pizze;
+		this.utente = utente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public int getCostoTotaleOrdine() {
+		return costoTotaleOrdine;
+	}
+
+	public void setCostoTotaleOrdine(int costoTotaleOrdine) {
+		this.costoTotaleOrdine = costoTotaleOrdine;
+	}
+
+	public boolean isClosed() {
+		return closed;
+	}
+
+	public void setClosed(boolean closed) {
+		this.closed = closed;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Set<Pizza> getPizze() {
+		return pizze;
+	}
+
+	public void setPizze(Set<Pizza> pizze) {
+		this.pizze = pizze;
+	}
+
+	public Utente getUtente() {
+		return utente;
+	}
+
+	public void setUtente(Utente utente) {
+		this.utente = utente;
+	}
+
+	public LocalDateTime getCreateDateTime() {
+		return createDateTime;
+	}
+
+	public void setCreateDateTime(LocalDateTime createDateTime) {
+		this.createDateTime = createDateTime;
+	}
+
+	public LocalDateTime getUpdateDateTime() {
+		return updateDateTime;
+	}
+
+	public void setUpdateDateTime(LocalDateTime updateDateTime) {
+		this.updateDateTime = updateDateTime;
+	}
+
+}
---
+++ src/main/java/it/prova/pizzastore/model/Pizza.java
@@ -0,0 +1,124 @@
+package it.prova.pizzastore.model;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "pizza")
+public class Pizza {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "descrizione")
+	private String descrizione;
+	@Column(name = "ingredienti")
+	private String ingredienti;
+	@Column(name = "prezzobase")
+	private int prezzoBase;
+	@Column(name = "attivo")
+	private boolean attivo;
+
+	@ManyToMany(fetch = FetchType.LAZY, mappedBy = "pizze")
+	private Set<Ordine> ordini;
+
+	@CreationTimestamp
+	private LocalDateTime createDateTime;
+	@UpdateTimestamp
+	private LocalDateTime updateDateTime;
+
+	public Pizza() {
+	}
+
+	public Pizza(String descrizione, String ingredienti, int prezzoBase) {
+		super();
+		this.descrizione = descrizione;
+		this.ingredienti = ingredienti;
+		this.prezzoBase = prezzoBase;
+	}
+
+	public Pizza(String descrizione, String ingredienti, int prezzoBase, boolean attivo, Set<Ordine> ordini) {
+		super();
+		this.descrizione = descrizione;
+		this.ingredienti = ingredienti;
+		this.prezzoBase = prezzoBase;
+		this.attivo = attivo;
+		this.ordini = ordini;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public String getIngredienti() {
+		return ingredienti;
+	}
+
+	public void setIngredienti(String ingredienti) {
+		this.ingredienti = ingredienti;
+	}
+
+	public int getPrezzoBase() {
+		return prezzoBase;
+	}
+
+	public void setPrezzoBase(int prezzoBase) {
+		this.prezzoBase = prezzoBase;
+	}
+
+	public boolean isAttivo() {
+		return attivo;
+	}
+
+	public void setAttivo(boolean attivo) {
+		this.attivo = attivo;
+	}
+
+	public Set<Ordine> getOrdini() {
+		return ordini;
+	}
+
+	public void setOrdini(Set<Ordine> ordini) {
+		this.ordini = ordini;
+	}
+
+	public LocalDateTime getCreateDateTime() {
+		return createDateTime;
+	}
+
+	public void setCreateDateTime(LocalDateTime createDateTime) {
+		this.createDateTime = createDateTime;
+	}
+
+	public LocalDateTime getUpdateDateTime() {
+		return updateDateTime;
+	}
+
+	public void setUpdateDateTime(LocalDateTime updateDateTime) {
+		this.updateDateTime = updateDateTime;
+	}
+}
