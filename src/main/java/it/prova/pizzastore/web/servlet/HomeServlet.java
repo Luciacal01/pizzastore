514e7e076fb8b0f2fae2138a65f4b903278bc4bb
Luciacal01 <luciacalabria80@gmail.com>
2022-06-03 17:07:50 +0200

    11° commit: creazione e implementazione login e logout Servlet

--- src/main/java/it/prova/pizzastore/web/servlet/HomeServlet.java
+++ src/main/java/it/prova/pizzastore/web/servlet/HomeServlet.java
@@ -13,12 +13,7 @@ public class HomeServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.getRequestDispatcher("login.jsp").forward(request, response);
 	}
 
 }
---
+++ src/main/java/it/prova/pizzastore/web/servlet/auth/LoginServlet.java
@@ -0,0 +1,60 @@
+package it.prova.pizzastore.web.servlet.auth;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+
+import it.prova.pizzastore.model.Ruolo;
+import it.prova.pizzastore.model.Utente;
+import it.prova.pizzastore.service.MyServiceFactory;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginServlet() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String loginInput = request.getParameter("inputUsername");
+		String passwordInput = request.getParameter("inputPassword");
+		
+		if(StringUtils.isEmpty(loginInput) || StringUtils.isEmpty(passwordInput)) {
+			request.setAttribute("errorMessage", "E' necessario riempire tutti i campi.");
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+			return;
+		}
+		
+		String destinazione=null;
+		
+		try {
+			Utente utenteInstance=MyServiceFactory.getUtenteServiceInstance().accedi(loginInput, passwordInput);
+			if(utenteInstance==null) {
+				request.setAttribute("errorMessage", "Utente non trovato.");
+				destinazione = "login.jsp";
+			}else {
+				request.setAttribute("userInfo", utenteInstance);
+				destinazione="home";
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			destinazione = "login.jsp";
+			request.setAttribute("errorMessage", "Attenzione! Si è verificato un errore.");
+		}
+		
+		request.getRequestDispatcher(destinazione).forward(request, response);
+		
+	}
+
+}
