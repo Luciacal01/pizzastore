17e1b00af9fdcb85200ac35160f06d9e3239d994
Luciacal01 <luciacalabria80@gmail.com>
2022-06-03 10:01:48 +0200

    7° commit: implementazione del listener

--- src/main/java/it/prova/pizzastore/web/listener/LocalEntityManagerFactoryListener.java
+++ src/main/java/it/prova/pizzastore/web/listener/LocalEntityManagerFactoryListener.java
@@ -9,6 +9,13 @@ import javax.persistence.PersistenceException;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import it.prova.pizzastore.model.Ruolo;
+import it.prova.pizzastore.model.StatoUtente;
+import it.prova.pizzastore.model.Utente;
+import it.prova.pizzastore.service.MyServiceFactory;
+import it.prova.pizzastore.service.RuoloService;
+import it.prova.pizzastore.service.UtenteService;
+
 //import it.prova.raccoltafilm.model.Ruolo;
 //import it.prova.raccoltafilm.model.StatoUtente;
 //import it.prova.raccoltafilm.model.Utente;
@@ -59,25 +66,45 @@ public class LocalEntityManagerFactoryListener implements ServletContextListener
 		}
 	}
 	
-//	private void initAdminUserAndRuoli() throws Exception {
-//		RuoloService ruoloServiceInstance = MyServiceFactory.getRuoloServiceInstance();
-//		UtenteService utenteServiceInstance = MyServiceFactory.getUtenteServiceInstance();
+	private void initAdminUserAndRuoli() throws Exception {
+		RuoloService ruoloServiceInstance = MyServiceFactory.getRuoloServiceInstance();
+		UtenteService utenteServiceInstance = MyServiceFactory.getUtenteServiceInstance();
 
-//		if (ruoloServiceInstance.cercaPerDescrizioneECodice("Administrator", "ROLE_ADMIN") == null) {
-//			ruoloServiceInstance.inserisciNuovo(new Ruolo("Administrator", "ROLE_ADMIN"));
-//		}
+		if (ruoloServiceInstance.cercaPerDescrizioneECodice("Administrator", "ROLE_ADMIN") == null) {
+			ruoloServiceInstance.inserisciNuovo(new Ruolo("Administrator", "ROLE_ADMIN"));
+		}
 
-//		if (ruoloServiceInstance.cercaPerDescrizioneECodice("Classic User", "ROLE_CLASSIC_USER") == null) {
-//			ruoloServiceInstance.inserisciNuovo(new Ruolo("Classic User", "ROLE_CLASSIC_USER"));
-//		}
+		if (ruoloServiceInstance.cercaPerDescrizioneECodice("Pizzaiolo User", "PIZZAIOLO_USER") == null) {
+			ruoloServiceInstance.inserisciNuovo(new Ruolo("Pizzaiolo User", "PIZZAIOLO_USER"));
+		}
+		
+		if (ruoloServiceInstance.cercaPerDescrizioneECodice("fattorino User", "FATTORINO_USER") == null) {
+			ruoloServiceInstance.inserisciNuovo(new Ruolo("fattorino User", "FATTORINO_USER"));
+		}
 
-//		if (utenteServiceInstance.findByUsernameAndPassword("admin", "admin") == null) {
-//			Utente admin = new Utente("admin", "admin", "Mario", "Rossi", new Date());
-//			admin.setStato(StatoUtente.ATTIVO);
-//			utenteServiceInstance.inserisciNuovo(admin);
-///			utenteServiceInstance.aggiungiRuolo(admin,
-//					ruoloServiceInstance.cercaPerDescrizioneECodice("Administrator", "ROLE_ADMIN"));
-///		}
-//	}
+		if (utenteServiceInstance.findByUsernameAndPassword("admin", "admin") == null) {
+			Utente admin = new Utente("admin", "admin", "Mario", "Rossi", new Date());
+			admin.setStato(StatoUtente.ATTIVO);
+			utenteServiceInstance.inserisciNuovo(admin);
+			utenteServiceInstance.aggiungiRuolo(admin,
+					ruoloServiceInstance.cercaPerDescrizioneECodice("Administrator", "ROLE_ADMIN"));
+		}
+		
+		if (utenteServiceInstance.findByUsernameAndPassword("pizzaiolouser", "pizzaiolouser") == null) {
+			Utente pizzaiolo = new Utente("pizzaiolouser", "pizzaiolouser", "Franco", "Cossi", new Date());
+			pizzaiolo.setStato(StatoUtente.ATTIVO);
+			utenteServiceInstance.inserisciNuovo(pizzaiolo);
+			utenteServiceInstance.aggiungiRuolo(pizzaiolo,
+					ruoloServiceInstance.cercaPerDescrizioneECodice("Pizzaiolo User", "PIZZAIOLO_USER"));
+		}
+		
+		if (utenteServiceInstance.findByUsernameAndPassword("fattorinouser", "fattorinouser") == null) {
+			Utente fattorino = new Utente("fattorinouser", "fattorinouser", "Giovanni", "Forte", new Date());
+			fattorino.setStato(StatoUtente.ATTIVO);
+			utenteServiceInstance.inserisciNuovo(fattorino);
+			utenteServiceInstance.aggiungiRuolo(fattorino,
+					ruoloServiceInstance.cercaPerDescrizioneECodice("fattorino User", "FATTORINO_USER"));
+		}
+	}
 
 }
