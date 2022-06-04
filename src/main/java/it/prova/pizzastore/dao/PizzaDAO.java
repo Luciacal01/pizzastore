5e5a37c0e7a0efbb458b313cf6375cf0e5a20032
Luciacal01 <luciacalabria80@gmail.com>
2022-06-04 16:41:35 +0200

    20° commit: aggiornamento findByExample Pizza

--- src/main/java/it/prova/pizzastore/dao/PizzaDAO.java
+++ src/main/java/it/prova/pizzastore/dao/PizzaDAO.java
@@ -2,10 +2,10 @@ package it.prova.pizzastore.dao;
 
 import java.util.List;
 
+
 import it.prova.pizzastore.model.Pizza;
 
 public interface PizzaDAO extends IBaseDAO<Pizza> {
 	
 	public List<Pizza> findByExample(Pizza example) throws Exception;
-
 }
--- src/main/java/it/prova/pizzastore/dao/PizzaDAOImpl.java
+++ src/main/java/it/prova/pizzastore/dao/PizzaDAOImpl.java
@@ -1,9 +1,15 @@
 package it.prova.pizzastore.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import org.apache.commons.lang3.StringUtils;
 
 import it.prova.pizzastore.model.Pizza;
 
@@ -54,7 +60,34 @@ public class PizzaDAOImpl implements PizzaDAO {
 	}
 	
 	public List<Pizza> findByExample(Pizza example) throws Exception{
-		return null;
+		Map<String, Object> paramaterMap = new HashMap<String, Object>();
+		List<String> whereClauses = new ArrayList<String>();
+
+		StringBuilder queryBuilder = new StringBuilder("select p from Pizza p where p.id = p.id ");
+
+		if (StringUtils.isNotEmpty(example.getDescrizione())) {
+			whereClauses.add(" p.descrizione  like :descrizione ");
+			paramaterMap.put("descrizione", "%" + example.getDescrizione() + "%");
+		}
+		if (StringUtils.isNotEmpty(example.getIngredienti())) {
+			whereClauses.add(" p.ingredienti like :ingredienti ");
+			paramaterMap.put("ingredienti", "%" + example.getIngredienti() + "%");
+		}
+		if (example.getPrezzoBase() > 0) {
+			whereClauses.add(" p.prezzobase =:prezzobase ");
+			paramaterMap.put("prezzobase", example.getPrezzoBase());
+		}
+
+		queryBuilder.append(!whereClauses.isEmpty() ? " and " : "");
+		queryBuilder.append(StringUtils.join(whereClauses, " and "));
+
+		TypedQuery<Pizza> typedQuery = entityManager.createQuery(queryBuilder.toString(), Pizza.class);
+
+		for (String key : paramaterMap.keySet()) {
+			typedQuery.setParameter(key, paramaterMap.get(key));
+		}
+
+		return typedQuery.getResultList();
 	}
 
 }
